21dd81f70115b602aaf4965c88777d74807b11d9
michfioravera <51338780+michfioravera@users.noreply.github.com>
2025-06-30 18:26:47 +0100

    Delete netlify/api.js

--- netlify/api.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-  const params = event.queryStringParameters;
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ timestamp: new Date().toISOString(), query: params })
-  };
-};
